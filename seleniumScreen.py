d2eb37db0b6fe6fd8ceb77074511fa168fd58e3d
ACER <locnguyen@mfunctions.com>
2023-05-05 23:41:30 +0700

    fix

--- seleniumScreen.py
+++ seleniumScreen.py
@@ -3,34 +3,35 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import FirefoxOptions
 
 def screenShotVideoYT(id,times):
-    driver = webdriver.Firefox()
+    options = FirefoxOptions()
+    options.binary_location = '/usr/bin/firefox'
+    driver = webdriver.Firefox(options=options, executable_path="/snap/bin/geckodriver")
     url = "https://www.youtube.com/embed/"+id+"?start="+str(times)
     # driver.get("https://www.youtube.com/watch?v=NXcR7fgo7vE&t=60s")
     driver.get(url)
     driver.find_element(By.TAG_NAME, "video")
-    print("HEHE")
-    # WebDriverWait(driver, 10).until(
-    #     EC.presence_of_element_located((By.CSS_SELECTOR, ".ytp-progress-bar-container"))
-    # )
-    # time.sleep(2)
-    # driver.execute_script("document.getElementsByTagName('video')[0].click()")
-    # time.sleep(2)
-    # driver.execute_script("""
-    #         (function (window, document) {
-    #          var canvas = document.createElement('canvas');
-    #          var video = document.querySelector('video');
-    #          var ctx = canvas.getContext('2d');
-    #          canvas.width = parseInt(video.offsetWidth);
-    #          canvas.height =  parseInt(video.offsetHeight);
-    #          ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    #          var a = document.createElement('a');
-    #          a.download = 'snap-' + canvas.width + 'x' + canvas.height + '-' + video.currentTime + '.jpg';
-    #          a.href = canvas.toDataURL('image/jpeg');
-    #          document.body.appendChild(a).click();
-    #          a.remove();
-    #         })(window, document)
-    #         """)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".ytp-progress-bar-container"))
+    )
+    time.sleep(2)
+    driver.execute_script("document.getElementsByTagName('video')[0].click()")
+    time.sleep(2)
+    driver.execute_script("""
+            (function (window, document) {
+             var canvas = document.createElement('canvas');
+             var video = document.querySelector('video');
+             var ctx = canvas.getContext('2d');
+             canvas.width = parseInt(video.offsetWidth);
+             canvas.height =  parseInt(video.offsetHeight);
+             ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+             var a = document.createElement('a');
+             a.download = 'snap-' + canvas.width + 'x' + canvas.height + '-' + video.currentTime + '.jpg';
+             a.href = canvas.toDataURL('image/jpeg');
+             document.body.appendChild(a).click();
+             a.remove();
+            })(window, document)
+            """)
 screenShotVideoYT("uYhT8FL9aHo","60")
