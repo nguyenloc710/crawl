f0050ff4a826185086552f593f5c36d1c196cf15
ACER <locnguyen@mfunctions.com>
2023-05-05 21:56:13 +0700

    test

--- seleniumScreen.py
+++ seleniumScreen.py
@@ -12,25 +12,26 @@ def screenShotVideoYT(id,times):
     # driver.get("https://www.youtube.com/watch?v=NXcR7fgo7vE&t=60s")
     driver.get(url)
     driver.find_element(By.TAG_NAME, "video")
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, ".ytp-progress-bar-container"))
-    )
-    time.sleep(2)
-    driver.execute_script("document.getElementsByTagName('video')[0].click()")
-    time.sleep(2)
-    driver.execute_script("""
-            (function (window, document) {
-             var canvas = document.createElement('canvas');
-             var video = document.querySelector('video');
-             var ctx = canvas.getContext('2d');
-             canvas.width = parseInt(video.offsetWidth);
-             canvas.height =  parseInt(video.offsetHeight);
-             ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-             var a = document.createElement('a');
-             a.download = 'snap-' + canvas.width + 'x' + canvas.height + '-' + video.currentTime + '.jpg';
-             a.href = canvas.toDataURL('image/jpeg');
-             document.body.appendChild(a).click();
-             a.remove();
-            })(window, document)
-            """)
+    print("HEHE")
+    # WebDriverWait(driver, 10).until(
+    #     EC.presence_of_element_located((By.CSS_SELECTOR, ".ytp-progress-bar-container"))
+    # )
+    # time.sleep(2)
+    # driver.execute_script("document.getElementsByTagName('video')[0].click()")
+    # time.sleep(2)
+    # driver.execute_script("""
+    #         (function (window, document) {
+    #          var canvas = document.createElement('canvas');
+    #          var video = document.querySelector('video');
+    #          var ctx = canvas.getContext('2d');
+    #          canvas.width = parseInt(video.offsetWidth);
+    #          canvas.height =  parseInt(video.offsetHeight);
+    #          ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    #          var a = document.createElement('a');
+    #          a.download = 'snap-' + canvas.width + 'x' + canvas.height + '-' + video.currentTime + '.jpg';
+    #          a.href = canvas.toDataURL('image/jpeg');
+    #          document.body.appendChild(a).click();
+    #          a.remove();
+    #         })(window, document)
+    #         """)
 screenShotVideoYT("uYhT8FL9aHo","60")
