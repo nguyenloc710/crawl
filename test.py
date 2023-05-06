99f25fba7acf21c5594104f5ed8c01e9d7bc29a6
ACER <locnguyen@mfunctions.com>
2023-05-06 10:29:51 +0700

    add binary

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.service import Service
 
 print("start")
+binary = FirefoxBinary('/usr/bin/firefox')
 options = webdriver.FirefoxOptions()
 options.headless = True
 
 service = Service("/home/ubuntu/driveSelenium/geckodriver")
-driver = webdriver.Firefox(options=options, service=service)
+driver = webdriver.Firefox(firefox_binary=binary, options=options, service=service)
 print("done drive")
 
 # Navigate to a webpage
