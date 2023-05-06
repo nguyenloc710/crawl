8dd47a123c69cf4d787c6020ac719f148dd6fa54
ACER <locnguyen@mfunctions.com>
2023-05-06 10:33:52 +0700

    ok

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 
 print("start")
-binary = '/usr/bin/firefox'
 options = webdriver.FirefoxOptions()
 options.headless = True
+options.add_argument("--headless")
 
 service = Service("/home/ubuntu/driveSelenium/geckodriver")
-driver = webdriver.Firefox(firefox_binary=binary, options=options, service=service)
+driver = webdriver.Firefox(options=options, service=service)
 print("done drive")
 
 # Navigate to a webpage
