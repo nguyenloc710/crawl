cd0115517f6369ccc598420c2cf744d5e8ef81f8
ACER <locnguyen@mfunctions.com>
2023-05-24 14:20:49 +0700

    done

--- app.py
+++ app.py
@@ -3,8 +3,5 @@ import time
 num = 1
 while True:
     num += 1
-    with open('test.txt', 'w') as file:
-        # Write the content to the file
-        file.write(str(num))
-        print(num)
-        time.sleep(1)
+    print(num)
+    time.sleep(1)
